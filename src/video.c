62615f88b1aca583ab1a8b16fd730ac54d3ab696
long <long@long-G41MT-S2P.(none)>
2013-05-17 16:04:28 +0800

    添加新文件.

---
+++ src/video.c
@@ -0,0 +1,14 @@
+
+
+
+#include "video.h"
+
+struct video{
+    int fd;
+    struct v4l2_format format;
+};
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ src/video.h
@@ -0,0 +1,14 @@
+#ifndef __VIDEO_H__
+#define __VIDEO_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <linux/videodev2.h>
+
+
+
+
+
+#endif
+

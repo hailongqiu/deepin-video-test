ed055a3fdc4359bcd7acecd19c8254837289eaf1
long <long@long-G41MT-S2P.(none)>
2013-05-17 17:42:08 +0800

    moidfy code.

--- src/video.c
+++ src/video.c
@@ -1,14 +1,102 @@
 
-
-
 #include "video.h"
 
-struct video{
-    int fd;
-    struct v4l2_format format;
-};
+
+int video_open(char *dev, Video *video)
+{
+    if (!dev)
+    {
+        dev = VIDEO_DEVICE_0;
+    }
+    video->fd = open(dev, O_RDWR);
+    //
+    puts("video_open...");
+    struct v4l2_input input;
+    struct v4l2_audio audio;
+    memset(&input, 0, sizeof(input));
+    memset(&audio, 0, sizeof(audio));
+
+    int index;
+    if (-1 == ioctl (video->fd, VIDIOC_G_INPUT, &index))
+    {
+        perror("错误...");
+        exit (EXIT_FAILURE);
+    }
+    input.index = index;
+    //
+    if (-1 == ioctl (video->fd, VIDIOC_ENUMINPUT, &input))
+    {
+        perror("video_enum..put");
+        exit (EXIT_FAILURE);
+    }
+    printf("input.index:%d\n", input.index);
+    printf("input.name:%s\n", input.name);
+
+    return video->fd;
+}
+
+void video_set_format(
+            Video *video, 
+            const Opt opt, 
+            const int stream_flag
+            )
+{
+    video->format.type = stream_flag;
+    video->format.fmt.pix.width = opt.width;
+    video->format.fmt.pix.height = opt.height;
+    video->format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+    //
+    if (ioctl(video->fd, VIDIOC_S_FMT, video->format) == -1)
+    {
+        perror("获取视频格式错误...");
+    }
+}
+
+void video_close(Video *video)
+{
+    close(video->fd);
+}
+
+
+void option_init(Opt *opt)
+{
+    opt->verbose = 0;
+    opt->width   = 640;
+    opt->height  = 480;
+}
 
 int main(int argc, char *argv[])
 {
+    Opt   opt;
+    Video video;
+    int stream_flag = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    // video_init();
+    //
+    option_init(&opt);
+    // 开发设备. video_open();
+    if (-1 == video_open(NULL, &video))
+    {
+        perror("/dev/videXX 打开错误!!... ...\n");
+        exit (-1);
+    }
+    // 获取设备信息.
+    memset(&video.format, 0, sizeof(video.format)); // 清除.
+    video.format.type = stream_flag;
+    video.format.fmt.pix.width = opt.width;
+    video.format.fmt.pix.height = opt.height;
+    video.format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+    //
+    if (ioctl(video.fd, VIDIOC_S_FMT, video.format) == -1)
+    {
+        perror("获取视频格式错误...");
+    }
+    //video_set_format(&video, opt, stream_flag);
+    // 测试.
+    printf("width:%d\n", video.format.fmt.pix.width);
+    // buffer_init();
+    // 关闭连接.
+    video_close(&video);
     return 0;
 }
+
+
--- src/video.h
+++ src/video.h
@@ -4,11 +4,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
 #include <linux/videodev2.h>
 
 
+#define VIDEO_DEVICE_0  "/dev/video0"
 
+typedef struct _opt    Opt;
+typedef struct _buffer Buffer;
+typedef struct _video  Video;
 
+struct _opt
+{
+    int verbose;
+    int width;
+    int height;
+};
+
+struct _buffer
+{
+    struct v4l2_requestbuffers req; // 请求.
+    struct v4l2_buffer query; // 获取.
+    struct {
+        void *start;
+        size_t length;
+    }*buf;
+};
+
+struct _video
+{
+    int fd;
+    struct v4l2_format format;
+    struct v4l2_capability capability;
+    Buffer buffer;
+};
+
+void option_init(Opt *opt); // 初始化 opt.
+int video_open(char *dev, Video *video);
+void video_close(Video *video);
+void video_set_format(Video *video, const Opt opt, const int stream_flag);
 
 #endif
 
